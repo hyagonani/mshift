2965a842aac8b255d9d97a751ea1b6b8cb9de514
hyagonani <hyagonani@gmail.com>
2026-08-12 09:05:58 -0300

    rota agendamento

---
+++ app/agendamento/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.redirect(
+    'https://calendly.com/sistemadeusinagemempresarial/sistema-de-usinagem-empresarial'
+  )
+}
